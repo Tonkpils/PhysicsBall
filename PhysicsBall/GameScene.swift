6dd8f26eb85df6c0c618e1d4ac38bb2a70cf6b38
Leo Correa <lcorr005@gmail.com>
2015-12-04 12:58:06 -0500

    Setup pinball in scene with physics body

--- PhysicsBall/GameScene.swift
+++ PhysicsBall/GameScene.swift
@@ -10,36 +10,25 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 45;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        self.setupScene()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+
     }
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+    }
+
+    func setupScene() {
+        backgroundColor = SKColor.whiteColor()
+
+        let ball = SKSpriteNode(imageNamed: "pinball.png")
+        ball.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        ball.size = CGSize(width: 20, height: 20)
+        addChild(ball)
+
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
     }
 }
